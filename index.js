931b69aa975d3d1a42d1363e0aa3c45891b5198f
LitanMolla <litanmolla9@gmail.com>
2026-07-12 11:43:12 +0600

    added static middleware for access files

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { createTodo } = require('./controllers/todoController')
 const app = express()
 app.use(express.json())
 app.use(cors())
+app.use('/upload',express.static('upload'))
 mongoose.connect('mongodb+srv://todoapp:todoapp@cluster0.eluznep.mongodb.net/todoapp?appName=Cluster0').then(() => { console.log(`Database connected successfully`) })
 const storage = multer.diskStorage({
     destination: (req,file,cb)=>{
