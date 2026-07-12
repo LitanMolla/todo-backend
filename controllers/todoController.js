3a39b774d2d2d520a644412f6d5b5ea5e1fa84f2
LitanMolla <litanmolla9@gmail.com>
2026-07-12 12:14:24 +0600

    added get all todos and delete todo controllers

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,11 +2,24 @@ const Todo = require('../models/todoModel')
 
 const createTodo = async (req, res) => {
     const { task, priority } = req.body
-    const fileType = req.file.mimetype.split('/')[0]
-    const fileUrl = req.file.path
+    const fileType = req?.file?.mimetype.split('/')[0]
+    const fileUrl = req?.file?.path
+    if (!task||!priority) {
+        return res.send({success: false, message: 'All feild are required'})
+    }
     const todo = new Todo({ task: task, priority: priority, path: {type: fileType, url: fileUrl} })
-    res.send(todo)
+    res.send({success: true, message: "Todo created successfully", data: todo})
     await todo.save()
 }
+const getTodos = async (req,res)=>{
+    const todos = await Todo.find({})
+    res.send({success: true, message: "Todo get succssfully", data: todos})
+}
+const deleteTodo = async (req,res)=>{
+    const {id} = req.params
+    await Todo.findByIdAndDelete({_id:id})
+    console.log(id)
+    res.send({success:true,message: "Todo Deleted"})
+}
 
-module.exports = { createTodo }
+module.exports = { createTodo , getTodos, deleteTodo}
