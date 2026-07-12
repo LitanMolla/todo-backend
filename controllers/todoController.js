37741e9ba2870b190476d4b3cee4d241ef4609a7
LitanMolla <litanmolla9@gmail.com>
2026-07-12 11:25:28 +0600

    added multer for upload filer

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,9 +2,11 @@ const Todo = require('../models/todoModel')
 
 const createTodo = async (req, res) => {
     const { task, priority } = req.body
-    const todo = new Todo({ task: task, priority: priority })
+    const fileType = req.file.mimetype.split('/')[0]
+    const fileUrl = req.file.path
+    const todo = new Todo({ task: task, priority: priority, path: {type: fileType, url: fileUrl} })
     res.send(todo)
-    console.log(todo)
+    await todo.save()
 }
 
 module.exports = { createTodo }
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const multer = require('multer')
 const { createTodo } = require('./controllers/todoController')
 const app = express()
-
 app.use(express.json())
 app.use(cors())
-
 mongoose.connect('mongodb+srv://todoapp:todoapp@cluster0.eluznep.mongodb.net/todoapp?appName=Cluster0').then(() => { console.log(`Database connected successfully`) })
+const storage = multer.diskStorage({
+    destination: (req,file,cb)=>{
+        cb(null,'./upload')
+    },
+    filename: (req,file,cb)=>{
+        const uniqueName = Date.now() + '-' + file.originalname
+        cb(null,uniqueName)
+    }
+})
+const upload = multer({storage:storage})
 
 app.get('/',(req,res)=>{res.send('Hello world')})
-app.post('/todo/create',createTodo)
+app.post('/todo/create',upload.single('image'),createTodo)
 app.listen(5000,()=>{console.log('Server is running')})
