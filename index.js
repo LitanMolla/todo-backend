bfc4a4bd5fd09d30b93da318e11741f00969a600
LitanMolla <litanmolla9@gmail.com>
2026-07-14 23:06:02 +0600

    moved upload codes

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const cors = require('cors')
 const multer = require('multer')
 const { createTodo, getTodos, deleteTodo, updateTodo } = require('./controllers/todoController')
 const dbConnect = require('./config/dbConfig')
+const upload = require('./utils/upload')
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use('/upload',express.static('upload'))
+app.use('/upload', express.static('uploads'))
 dbConnect()
-const storage = multer.diskStorage({
-    destination: (req,file,cb)=>{
-        cb(null,'./upload')
-    },
-    filename: (req,file,cb)=>{
-        const uniqueName = Date.now() + '-' + file.originalname
-        cb(null,uniqueName)
-    }
-})
-const upload = multer({storage:storage})
 
-app.get('/',(req,res)=>{res.send('Hello world')})
-app.post('/todo/create',upload.single('image'),createTodo)
-app.get('/todo',getTodos)
-app.delete('/todo/delete/:id',deleteTodo)
-app.post('/todo/update/:id',updateTodo)
-app.listen(5000,()=>{console.log('Server is running')})
+app.get('/', (req, res) => { res.send('Hello world') })
+app.post('/todo/create', upload.single('image'), createTodo)
+app.get('/todo', getTodos)
+app.delete('/todo/delete/:id', deleteTodo)
+app.post('/todo/update/:id', updateTodo)
+app.listen(5000, () => { console.log('Server is running') })
