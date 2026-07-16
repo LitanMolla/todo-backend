8df66110719d713da7ed4908148a51645e04ad98
LitanMolla <litanmolla9@gmail.com>
2026-07-16 06:58:42 +0600

    code updated with try catch

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -2,33 +2,49 @@ const Todo = require('../models/todoModel')
 
 const createTodo = async (req, res) => {
     const { task, priority } = req.body
-    const fileType = req?.file?.mimetype.split('/')[0]
-    const fileUrl = req?.file?.path
-    if (!task||!priority) {
-        return res.send({success: false, message: 'All feild are required'})
+    try {
+        const fileType = req?.file?.mimetype.split('/')[0]
+        const fileUrl = req?.file?.path
+        if (!task || !priority) {
+            return res.status(400).json({ success: false, message: 'All feild are required' })
+        }
+        const todo = new Todo({ task: task, priority: priority, path: { type: fileType, url: fileUrl } })
+        await todo.save()
+        res.status(201).json({ success: true, message: "Todo created successfully", data: todo })
+    } catch (error) {
+        return res.status(400).json({ success: false, message: error.message })
     }
-    const todo = new Todo({ task: task, priority: priority, path: {type: fileType, url: fileUrl} })
-    res.send({success: true, message: "Todo created successfully", data: todo})
-    await todo.save()
 }
-const getTodos = async (req,res)=>{
-    const todos = await Todo.find({})
-    res.send({success: true, message: "Todo get succssfully", data: todos})
+const getTodos = async (req, res) => {
+    try {
+        const todos = await Todo.find({})
+        res.status(200).json({ success: true, message: "Todo get succssfully", data: todos })
+    } catch (error) {
+        return res.status(400).json({ success: false, message: error.message })
+    }
 }
-const deleteTodo = async (req,res)=>{
-    const {id} = req.params
-    await Todo.findByIdAndDelete({_id:id})
-    res.send({success:true,message: "Todo Deleted"})
+const deleteTodo = async (req, res) => {
+    const { id } = req.params
+    try {
+        await Todo.findByIdAndDelete({ _id: id })
+        res.send({ success: true, message: "Todo Deleted" })
+    } catch (error) {
+        return res.status(400).json({ success: false, message: error.message })
+    }
 }
-const updateTodo = async = (req,res)=>{
-    const {id} = req.params
+const updateTodo = async (req, res) => {
+    const { id } = req.params
     const { task, priority } = req.body
     const fileType = req?.file?.mimetype.split('/')[0]
     const fileUrl = req?.file?.path
-    if (!task||!priority) {
-        return res.send({success: false, message: 'All feild are required'})
+    if (!task || !priority) {
+        return res.status(400).json({ success: false, message: 'All feild are required' })
     }
-    Todo.findByIdAndUpdate({_id:id},{ task: task, priority: priority, path: {type: fileType, url: fileUrl} })
-    res.send({success: true, message: "Todo updated successfully"})
+    try {
+        const data = await Todo.findByIdAndUpdate({ _id: id }, { task: task, priority: priority, path: { type: fileType, url: fileUrl } })
+        res.status(200).json({ success: true, message: "Todo updated successfully" })
+    } catch (error) {
+        return res.status(400).json({ success: false, message: error.message })
+    }
 }
-module.exports = { createTodo , getTodos, deleteTodo,updateTodo}
+module.exports = { createTodo, getTodos, deleteTodo, updateTodo }
